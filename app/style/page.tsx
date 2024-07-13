1086f071d3db9d589d879045e7653a69efe97c0e
blackicon-eth <mattiaverdecchi@yahoo.it>
2024-07-13 02:59:48 +0200

    Removed button

--- app/style/page.tsx
+++ app/style/page.tsx
@@ -5,9 +5,7 @@ import "../globals.css";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Button buttonText="Medium Rounded Button" rounded="md" color="cyan" />
-      </div>
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
     </main>
   );
 }
