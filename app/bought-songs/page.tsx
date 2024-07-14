f186c9e6e98c5fd0fe6ef915a9954f6973b1a49a
blackicon-eth <mattiaverdecchi@yahoo.it>
2024-07-14 05:43:15 +0200

    Added tables

--- app/bought-songs/page.tsx
+++ app/bought-songs/page.tsx
@@ -1,28 +1,21 @@
 "use client";
-
 import Link from "next/link";
 
 export default function Home() {
   // Mock table elements
   const tableElements = [
-    { title: "Si fa calcio", label: "Pop", price: "12", cid: "bafybeiaeax3inmgpavd5x77ckvkhhugei75xwvp7eaily5clzqptalvnfy" },
+    { title: "Come mai", label: "Pop", price: "32", cid: "bafybeiaeax3inmgpavd5x77ckvkhhugei75xwvp7eaily5clzqptalvnfy" },
     {
-      title: "Fly me to the moon",
-      label: "Jazz",
-      price: "23",
-      cid: "bafkreignxgnhx2mrbz3y3ulrp6wfx24yavv2kgfgrz5dcacdeqpibkxj7u",
-    },
-    {
-      title: "Don't stop me now",
-      label: "Rock",
-      price: "13",
+      title: "Rock me Amadeus",
+      label: "Disco/Pop",
+      price: "7",
       cid: "bafkreignxgnhx2mrbz3y3ulrp6wfx24yavv2kgfgrz5dcacdeqpibkxj7u",
     },
   ];
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col z-10 w-full max-w-8xl items-start justify-start font-mono text-sm lg:flex">
-        <div className="pl-3 pt-5 pb-4 text-black font-semibold text-4xl underline">Your content</div>
+        <div className="pl-3 pt-5 pb-4 text-black font-semibold text-4xl underline">Owned songs</div>
         <div className="flex flex-col w-full mt-5 items-start text-3xl text-black font-semibold bg-emerald-100 border-black border-2 rounded-lg">
           <div className="flex flex-row justify-between border-b-4 border-black w-full">
             <div className="border-r-2 border-black pl-3 p-4 text-center items-center w-full">Title</div>
